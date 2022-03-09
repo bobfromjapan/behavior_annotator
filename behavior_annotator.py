6f58c8a340eace647efee290cb4d650e0fa1980e
unknown <bobfromjapandev@gmail.com>
2022-03-09 21:23:43 +0900

    update napari version

--- behavior_annotator.py
+++ behavior_annotator.py
@@ -21,72 +21,75 @@ annotations = np.array([LABELS[0]]*len(vr))
 flag_exist = False
 flag_frame = 0
 
-with napari.gui_qt():
-    viewer = napari.Viewer()
-    image_layer = viewer.add_image(vr, rgb=True)
+viewer = napari.Viewer()
 
-    @viewer.bind_key('f')
-    def set_start_flag(event=None):
-        global flag_exist
-        global flag_frame
 
-        if flag_exist:
-            print('flag already exist!!')
+@viewer.bind_key('f')
+def set_start_flag(event=None):
+    global flag_exist
+    global flag_frame
+
+    if flag_exist:
+        print('flag already exist!!')
+    else:
+        flag_frame = image_layer._slice_indices[0]
+        flag_exist = True
+        print("start flag set to frame:", image_layer._slice_indices[0])
+
+@viewer.bind_key('1')
+def annotate_A(event=None):
+    global flag_exist
+    global flag_frame
+    if flag_exist:
+        if flag_frame > image_layer._slice_indices[0]:
+            print("go to the later frame than the start flag.")
+            show_globals()
         else:
-            flag_frame = image_layer._slice_indices[0]
-            flag_exist = True
-            print("start flag set to frame:", image_layer._slice_indices[0])
+            print(
+                flag_frame, "to", image_layer._slice_indices[0], "are annotated to ", LABELS[1])
+            annotations[flag_frame:image_layer._slice_indices[0]] = LABELS[1]
+            flag_exist = False
+    else:
+        print("need to set the start flag.")
 
-    @viewer.bind_key('1')
-    def annotate_A(event=None):
-        global flag_exist
-        global flag_frame
-        if flag_exist:
-            if flag_frame > image_layer._slice_indices[0]:
-                print("go to the later frame than the start flag.")
-                show_globals()
-            else:
-                print(
-                    flag_frame, "to", image_layer._slice_indices[0], "are annotated to ", LABELS[1])
-                annotations[flag_frame:image_layer._slice_indices[0]] = LABELS[1]
-                flag_exist = False
+@viewer.bind_key('2')
+def annotate_B(event=None):
+    global flag_exist
+    global flag_frame
+    if flag_exist:
+        if flag_frame > image_layer._slice_indices[0]:
+            print("go to the later frame than the start flag.")
+            show_globals()
         else:
-            print("need to set the start flag.")
+            print(
+                flag_frame, "to", image_layer._slice_indices[0], "are annotated to ", LABELS[2])
+            annotations[flag_frame:image_layer._slice_indices[0]] = LABELS[2]
+            flag_exist = False
+    else:
+        print("need to set the start flag.")
 
-    @viewer.bind_key('2')
-    def annotate_B(event=None):
-        global flag_exist
-        global flag_frame
-        if flag_exist:
-            if flag_frame > image_layer._slice_indices[0]:
-                print("go to the later frame than the start flag.")
-                show_globals()
-            else:
-                print(
-                    flag_frame, "to", image_layer._slice_indices[0], "are annotated to ", LABELS[2])
-                annotations[flag_frame:image_layer._slice_indices[0]] = LABELS[2]
-                flag_exist = False
-        else:
-            print("need to set the start flag.")
+@viewer.bind_key('3')
+def show_globals(event=None):
+    global flag_frame
+    global flag_exist
+    print("flag:", flag_exist)
+    print("pos:", flag_frame)
 
-    @viewer.bind_key('3')
-    def show_globals(event=None):
-        global flag_frame
-        global flag_exist
-        print("flag:", flag_exist)
-        print("pos:", flag_frame)
+@viewer.bind_key('4')
+def delete_flag(event=None):
+    global flag_exist
+    print("delete_flag!")
+    flag_exist = False
 
-    @viewer.bind_key('4')
-    def delete_flag(event=None):
-        global flag_exist
-        print("delete_flag!")
-        flag_exist = False
+@viewer.bind_key('5')
+def check_label(event=None):
+    print(annotations)
 
-    @viewer.bind_key('5')
-    def check_label(event=None):
-        print(annotations)
+@viewer.bind_key('0')
+def save_label(event=None):
+    print("save!")
+    pd.DataFrame(annotations).to_csv(output_name)
 
-    @viewer.bind_key('0')
-    def save_label(event=None):
-        print("save!")
-        pd.DataFrame(annotations).to_csv(output_name)
+image_layer = viewer.add_image(vr, rgb=True)
+
+napari.run()
